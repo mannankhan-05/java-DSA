c4ee11e18728374720159b0aa0a27a210589dd65
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-08-28 03:53:08 -0400

    example code of bubble sort algorithm to sort an array in descending order

--- DSA/src/bubbleSortAscending.java
+++ DSA/src/bubbleSortAscending.java
@@ -8,7 +8,7 @@ public class bubbleSortAscending {
     }
 
     public static void main(String[] args){
-        int arr[] = {7, 8, 3, 1, 2};
+        int arr[] = {7, 3, 5, 1, 2};
 
         // bubble sort for ascending order
         for(int i = 0; i < arr.length - 1; i++){
---
+++ DSA/src/bubbleSortDescending.java
@@ -0,0 +1,26 @@
+
+public class bubbleSortDescending {
+    public static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int arr[] = {3, 60, 1, 34, 2, 8, 9};
+
+        // bubble sort for descending order
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 0; j < arr.length - 1; j++){
+                if(arr[j] < arr[j+1]){
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArr(arr);
+    }
+}
