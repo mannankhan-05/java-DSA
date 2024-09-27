e7637b5cc1c298f7e33fda32ed3378b824ca6a19
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-09-27 06:57:13 -0400

    added a problem of BST (paths to leaf)

--- DSA/src/DS_binarySearchTree.java
+++ DSA/src/DS_binarySearchTree.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class DS_binarySearchTree {
     static class Node{
@@ -119,6 +120,32 @@ public class DS_binarySearchTree {
         }
     }
 
+    // question : root to leaf path
+    public static void printPathFromRootToLeaf(Node root, ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+
+        path.add(root.data);    // adding the root to the arraylist
+
+        // leaf
+        if(root.left == null && root.right == null){
+            printPath(path);
+        }
+        else{  // non-leaf node
+            printPathFromRootToLeaf(root.left, path);
+            printPathFromRootToLeaf(root.right, path);
+        }
+       path.remove(path.size() - 1);
+    }
+
+    public static void printPath(ArrayList<Integer> path){
+        for(int i = 0; i < path.size(); i++){
+            System.out.print(path.get(i) + " -> ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args){
         int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
@@ -135,5 +162,9 @@ public class DS_binarySearchTree {
         System.out.println();
 
         printInRange(root, 4, 11);
+        System.out.println();
+
+        System.out.println("Paths from root to leaf : ");
+        printPathFromRootToLeaf(root, new ArrayList<>());
     }
 }
